3f2983593bc491df53835c2d30889ac18796c3bc
Goszila <Goszila@DURIAN>
2017-09-12 11:38:06 +0700

    HAHA

--- src/Hello.java
+++ src/Hello.java
@@ -5,6 +5,18 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 		System.out.println("Hello Komek");
+		System.out.println("<==3");
+		System.out.println("<==3");
+		System.out.println("<==3");
+		System.out.println("<==3");
+		System.out.println("<==3");
+		System.out.println("<==3");
+		System.out.println("<==3");
+		System.out.println("<==3");
+		System.out.println("<==3");
+		System.out.println("<==3");
+		System.out.println("<==3");
+		
 	}
 
 }
