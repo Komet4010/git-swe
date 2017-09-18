310cdd9d7ef870c9911b37d0f613c912546a5bcf
Goszila <Goszila@DURIAN>
2017-09-18 13:48:25 +0700

    calculate

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,9 +1,19 @@
-
+import java.util.Scanner; 
 public class Calculate {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Input number");
+		int number,num;
+		number = keyboard.nextInt();
+		num= number%2;
 
+		if (num ==0)
+		System.out.println("àÅ¢¤Ùè");
+		else
+		System.out.println("àÅª¤Õè");
+		System.out.println("Hello");
 	}
 
 }
